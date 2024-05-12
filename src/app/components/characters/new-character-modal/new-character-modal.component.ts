21698fead40dead6c50ab888feeb133835c95113
Gabrielnolasco <gabrielncostasouza@outlook.com>
2024-05-11 22:58:50 -0300

    Colocando os dados para cadastro da ficha (Apresenta alguns erros)

--- src/app/components/characters/new-character-modal/new-character-modal.component.ts
+++ src/app/components/characters/new-character-modal/new-character-modal.component.ts
@@ -31,3 +31,61 @@ export class NewCharacterModalComponent {
 
   @Output() closeModal: EventEmitter<any> = new EventEmitter();
 }
+
+export class NewCharacterModalComponent_segunda_parte {
+  newCharacterFormGroup: FormGroup = new FormGroup({
+    // Os dados abaixo serão dos atributos --> Todos recebem Number até 18
+    Forca: new FormControl(''),
+    Destreza: new FormControl(''),
+    Constituicao: new FormControl(''),
+    Inteligencia: new FormControl(''),
+    Sabedoria: new FormControl(''),
+    Carisma: new FormControl(''),
+    // Os dados abaixo são as perícias --> Receberão Verdadeiro ou Falso
+    Acrobacia: new FormControl(''),
+    Arcanismo: new FormControl(''),
+    Atletismo: new FormControl(''),
+    Atuacao: new FormControl(''),
+    Enganacao: new FormControl(''),
+    Furtividade: new FormControl(''),
+    História: new FormControl(''),
+    Intimidacao: new FormControl(''),
+    Intuicao: new FormControl(''),
+    Investigacao: new FormControl(''),
+    Lidar_com_Animais: new FormControl(''),
+    Medicina: new FormControl(''),
+    Natureza: new FormControl(''),
+    Percepcao: new FormControl(''),
+    Persuasao: new FormControl(''),
+    Prestidigitacao: new FormControl(''),
+    Religiao: new FormControl(''),
+    Sobrevivencia: new FormControl(''),
+  });
+
+  currentCharacterData = {
+    Forca: '',
+    Destreza: '',
+    Constituicao: '',
+    Inteligencia: '',
+    Sabedoria: '',
+    Carisma: '',
+    Acrobacia: '',
+    Arcanismo: '',
+    Atletismo: '',
+    Atuacao: '',
+    Enganacao: '',
+    Furtividade: '',
+    História: '',
+    Intimidacao: '',
+    Intuicao: '',
+    Investigacao: '',
+    Lidar_com_Animais: '',
+    Medicina: '',
+    Natureza: '',
+    Percepcao: '',
+    Persuasao: '',
+    Prestidigitacao: '',
+    Religiao: '',
+    Sobrevivencia: '',
+  }
+}
